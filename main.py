f1b408d84e99da9849c62331b5503bfd96da16ba
ArtemyNeborsky <artemiinebo00@gmail.com>
2025-12-06 13:05:20 +0300

    update main.py

--- main.py
+++ main.py
@@ -2,44 +2,55 @@ import requests
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 api_key = os.getenv("TOKEN")
 
-def get_games(api):
-    payload = {"key": api_key, "genres": "strategy", "metacritic": 100, "tags": "multiplayer"}
-    url = "https://api.rawg.io/api/games"
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-    games_dict = []
-    for num in range(10):
-        game_name = response.json()["results"][num]["name"]
-        game_date = response.json()["results"][num]["released"]
-        game_id = response.json()["results"][num]["id"]
-        game_link = f"https://rawg.io/games/{response.json()["results"][num]["slug"]}"
-        game_screenshots = ""
-        for x in range(len(response.json()["results"][num]["short_screenshots"])):
-            game_screenshot = response.json()["results"][num]["short_screenshots"][x]["image"]
-            game_screenshots = game_screenshots + f" {game_screenshot}\n"
-        game_stores = ""
-        game = {
-            "name": game_name,
-            "date": game_date,
-            "link": game_link,
-            "screenshots": game_screenshots,
-            "stores": game_stores
-        }
-        games_dict.append(game)
-    return games_dict
+
+def get_games_list():
+    params = {
+        "key": api_key,
+        "genres": "strategy",
+        "tags": "multiplayer",
+        "metacritic": "80,100",
+        "page_size": 10
+    }
+    response = requests.get("https://api.rawg.io/api/games", params=params)
+    return response.json()["results"]
+
+
+def get_game_screenshots(game):
+    screenshots = []
+    for screenshot in game.get("short_screenshots"):
+        screenshots.append(screenshot["image"])
+    return screenshots
+
+
+def get_stores_for_game(game_id):
+    url = f"https://api.rawg.io/api/games/{game_id}/stores"
+    params = {"key": api_key}
+
+    response = requests.get(url, params=params)
+    stores = response.json()["results"]
+
+    store_list = []
+    for store in stores:
+        store_list.append(f"{store['url']}")
+    return "\n".join(store_list)
 
 
 def main():
-    for numb in range(len(get_games(api_key))):
-        print("Название игры:", get_games(api_key)[numb]["name"])
-        print("Дата выхода:", get_games(api_key)[numb]["date"])
-        print("Ссылка на игру:", get_games(api_key)[numb]["link"])
-        print("Магазины: -")
-        print("Скриншоты:", get_games(api_key)[numb]["screenshots"])
+    games_data = get_games_list()
+    for game in games_data:
+        screenshots = get_game_screenshots(game)
+        stores = get_stores_for_game(game["id"])
+
+        print(f"\nИгра: {game['name']}")
+        print(f"Дата выхода: {game['released']}")
+        print(f"Ссылка: https://rawg.io/games/{game['slug']}")
+        print("Скриншоты из игры:")
+        for screenshot in screenshots:
+            print(f"{screenshot}")
+        print(f"Где купить:\n{stores}")
 
 
 if __name__ == "__main__":
